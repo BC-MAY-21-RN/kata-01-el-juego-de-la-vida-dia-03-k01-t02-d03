940bfa32cb12c8e89bd7f9b5e5d5c3361365be9b
carloseag1609 <carloseag1233@gmail.com>
2021-06-03 12:56:25 -0500

    Test in board matrix generation with Cell objects,
    Co-authored-by: yovanha <yfajardo0@ucol.mx>
    Co-authored-by: cortezroberto <cortezrobertov@gmail.com>
    Co-authored-by: CarlosAG1609 <carloseag1233@gmail.com>

--- src/models/Board.js
+++ src/models/Board.js
@@ -1,3 +1,6 @@
+const Cell = require('./Cell');
+const { DEAD, ALIVE } = require('../states')
+
 module.exports = class Board {
   constructor(rows, columns, stringInput) {
     this.rows = rows;
@@ -9,5 +12,13 @@ module.exports = class Board {
   generateMatrixBoard() {
     this.board = this.stringInput.split('\n');
     this.board = this.board.map(row => row.split(''));
+  }
+
+  fillMatrixWithCells() {
+    this.board = this.board.map(row => {
+      return row.map(col => {
+        return col === DEAD ? new Cell(DEAD) : new Cell(ALIVE)
+      })
+    });
   }
 }
--- tests/models/Board.test.js
+++ tests/models/Board.test.js
@@ -1,4 +1,5 @@
 const Board = require('../../src/models/Board');
+const Cell = require('../../src/models/Cell');
 
 describe('Tests in Board class', () => {
   test('should fill the matrix with the string input', () => {
@@ -13,6 +14,17 @@ describe('Tests in Board class', () => {
     for (let i = 0; i < board.board.length; i++) {
       for (let j = 0; j < board.board[i].length; j++) {
         expect(board.board[i][j]).toBe(expectedOutput[i][j]);
+      }
+    }
+  });
+
+  test('should fill the matrix with cell objects', () => {
+    const board = new Board(4, 8, '.......\n....*...\n...**...\n........\n');
+    board.generateMatrixBoard();
+    board.fillMatrixWithCells();
+    for (let i = 0; i < board.board.length; i++) {
+      for (let j = 0; j < board.board[i].length; j++) {
+        expect(board.board[i][j]).toBeInstanceOf(Cell);
       }
     }
   });
